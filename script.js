9e048ff86b7b2860cbe7045c8166dc6ac7a60777
Abdulvip <ahamdidueen@outlook.com>
2020-08-17 02:02:44 -0400

    fbi

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let name = prompt("What is your name");
+
+if (name === "layann") {
+  document.write("Such a beautiful name :)");
+} else if (name === "Razaan") {
+  document.write("Such a beautiful name :)");
+} else if (name === "razaan") {
+  document.write("Such a beautiful name :)");
+} else if (name === "Layann") {
+  document.write("Such a beautiful name :)");
+} else if (name === "tagea") {
+  document.write(`Hello mother`);
+} else if (name === "Tagea") {
+  document.write(`Hello mother`);
+} else {
+  document.write(` Hello ${name} *-*`);
+}
